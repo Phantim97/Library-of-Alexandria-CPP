666ce606bd78c543fccdc0d8e9257a0b9fed6338
Tim Coelho <timothycoelho@gmail.com>
2021-10-08 22:34:46 -0400

    Create create_window.cpp

---
+++ Windows/Win32 API/create_window.cpp
@@ -0,0 +1,205 @@
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
+#include <windows.h>
+/*
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+	}
+	return 0;
+
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+	// Register the window class.
+	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+	WNDCLASS wc = {};
+
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	// Create the window.
+
+	HWND hwnd = CreateWindowEx(
+		0,                              // Optional window styles.
+		CLASS_NAME,                     // Window class
+		L"Sample Window",    // Window text
+		WS_OVERLAPPEDWINDOW,            // Window style
+
+										// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+		NULL,       // Parent window    
+		NULL,       // Menu
+		hInstance,  // Instance handle
+		NULL        // Additional application data
+	);
+
+	if (hwnd == NULL)
+	{
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+
+	// Run the message loop.
+
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}*/
+template <class DERIVED_TYPE>
+class BaseWindow
+{
+public:
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		DERIVED_TYPE *pThis = NULL;
+
+		//Set Pointer on Create (take from lParam)
+		if (uMsg == WM_NCCREATE)
+		{
+			CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
+			pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
+			SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
+
+			pThis->m_hwnd = hwnd;
+		}
+		else //get the State from the Long Ptr
+		{
+			pThis = (DERIVED_TYPE*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+		}
+		if (pThis) // if you get the state as a message handle it
+		{
+			return pThis->HandleMessage(uMsg, wParam, lParam);
+		}
+		else 
+		{
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+
+	BaseWindow() : m_hwnd(NULL) { }
+
+	BOOL Create(
+		PCWSTR lpWindowName,
+		DWORD dwStyle,
+		DWORD dwExStyle = 0,
+		int x = CW_USEDEFAULT,
+		int y = CW_USEDEFAULT,
+		int nWidth = CW_USEDEFAULT,
+		int nHeight = CW_USEDEFAULT,
+		HWND hWndParent = 0,
+		HMENU hMenu = 0
+	)
+	{
+		WNDCLASS wc = { 0 };
+
+		wc.lpfnWndProc = DERIVED_TYPE::WindowProc;
+		wc.hInstance = GetModuleHandle(NULL);
+		wc.lpszClassName = ClassName();
+
+		RegisterClass(&wc);
+
+		m_hwnd = CreateWindowEx(
+			dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
+			nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
+		);
+
+		return (m_hwnd ? TRUE : FALSE);
+	}
+
+	HWND Window() const { return m_hwnd; }
+
+protected:
+
+	virtual PCWSTR  ClassName() const = 0;
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
+
+	HWND m_hwnd;
+};
+
+class MainWindow : public BaseWindow<MainWindow>
+{
+public:
+	PCWSTR  ClassName() const { return L"Sample Window Class"; }
+	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+};
+
+LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(m_hwnd, &ps);
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 9)); // change the color of the window by adding to COLOR_WINDOW
+		EndPaint(m_hwnd, &ps);
+	}
+	return 0;
+
+	case WM_CLOSE: //Dialog Box on X out
+		if (MessageBox(m_hwnd, L"Are you sure you want to Quit?", L"Quit Menu", MB_OKCANCEL) == IDOK)
+		{
+			DestroyWindow(m_hwnd);
+		}
+		return 0;
+
+	default:
+		return DefWindowProc(m_hwnd, uMsg, wParam, lParam); //default procedure for any message
+	}
+	return TRUE;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+	MainWindow win;
+
+	//Create a new window
+	if (!win.Create(L"Learn to Program Windows", WS_OVERLAPPEDWINDOW))
+	{
+		return 0;
+	}
+
+	ShowWindow(win.Window(), nCmdShow); //Display
+
+	// Run the message loop.
+
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
