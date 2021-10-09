1136053bd74414c3597abcfd380cc7cf975d4379
Tim Coelho <timothycoelho@gmail.com>
2021-10-08 21:56:54 -0400

    Update cmd_capture.cpp

--- Windows/cmd/cmd_capture.cpp
+++ Windows/cmd/cmd_capture.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+#include <cassert>
+#include <string>
 
+int main()
+{
+	std::cout << "Original: ";
+	std::string cap;
+	char buffer[256];
+	memset(buffer, 0, 256);
+
+	FILE* pipe = _popen("ipconfig", "r");
+	if (!pipe)
+	{
+		std::cout << "Failed to open pipe\n";
+		assert(false);
+	}
+
+	while (fgets(buffer, sizeof(buffer), pipe) != NULL)
+	{
+		cap.append(buffer);
+	}
+
+	system("cls");
+	std::cout << "Captured: ";
+	std::cout << cap << '\n';
+	system("pause");
+
+	return 0;
+}
