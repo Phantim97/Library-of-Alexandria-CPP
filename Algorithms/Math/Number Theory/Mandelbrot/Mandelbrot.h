d27595e21f100ac192ff614414710971c0080634
Tim Coelho <timothycoelho@gmail.com>
2021-10-08 23:04:02 -0400

    Create Mandelbrot.h

---
+++ Algorithms/Math/Number Theory/Mandelbrot/Mandelbrot.h
@@ -0,0 +1,8 @@
+class Mandelbrot 
+{
+		Mandelbrot() = default;
+
+		static const int MAX_ITERATIONS = 1000; //rendering quality
+
+		static int getIterations(double x, double y); //no instance varuables needed
+	};
