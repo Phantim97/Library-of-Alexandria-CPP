8d3c4f8a2871956cc69fbc42915e2f2f0885c80f
Tim Coelho <timothycoelho@gmail.com>
2022-11-28 20:15:18 -0500

    Create string_to_integer.cpp

---
+++ Algorithms/General Comp Sci/Recursion/string_to_integer.cpp
@@ -0,0 +1,11 @@
+int str_to_int(char* str)
+{
+    if (*str == '\0') //Base case
+    {
+        return 0;
+    }
+
+    int last_digit = *str - '0';
+    int small_number = str_to_int(str + 1); //Induction Hypothesis
+    return small_number * 10 + last_digit;
+}
