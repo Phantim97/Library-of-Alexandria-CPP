418f8bc553d5095da6a5b72d7c9e0f4ca20821f4
Tim Coelho <timothycoelho@gmail.com>
2022-11-28 20:14:39 -0500

    Create print_subsequences.cpp

---
+++ Algorithms/General Comp Sci/Recursion/print_subsequences.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void print_all_subsequences(char* str)
+{
+    if (*str == '\0') //Base case
+    {
+        std::cout << '\n';
+        return;
+    }
+
+    print_all_subsequences(str + 1); //Induction Hypothesis
+
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        std::cout << str[i];
+    }
+  
+    std::cout << '\n';
+}
