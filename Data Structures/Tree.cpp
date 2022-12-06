e8d6f02a0fad6c083288def6292652df4fe4dc04
Tim Coelho <timothycoelho@gmail.com>
2022-12-05 21:53:52 -0500

    Added driver code

--- Data Structures/Tree.cpp
+++ Data Structures/Tree.cpp
@@ -1,4 +1,3 @@
-#include <vector>
 
 template <typename T>
 class TreeNode
@@ -21,7 +20,7 @@ public:
         }
     }
 
-    void insert(T data)
+    void insert(const T data)
     {
         TreeNode<T>* node = new TreeNode<T>(data);
         children.push_back(node);
@@ -32,8 +31,48 @@ public:
         children.push_back(node);
     }
 
-    T operator[](int idx)
+    T data() const
+    {
+        return data_;
+    }
+    
+    size_t size() const
+    {
+        return children.size();
+    }
+
+    TreeNode<T>* operator[](const int idx)
     {
         return children[idx];
     }
 };
+
+void print(TreeNode<int>* root)
+{
+    std::cout << root->data() << '\n';
+    
+    for (int i = 0; i < root->size(); i++)
+    {
+        print(root->operator[](i));
+    }
+}
+
+int main()
+{
+    TreeNode<int>* root = new TreeNode<int>(1);
+    TreeNode<int>* node1 = new TreeNode<int>(2);
+    TreeNode<int>* node2 = new TreeNode<int>(3);
+    TreeNode<int>* node3 = new TreeNode<int>(4);
+
+    root->insert(node1);
+    root->insert(node2);
+    root->insert(node3);
+
+    root->insert(5);
+
+    print(root);
+
+    delete root;
+
+    return 0;
+}
