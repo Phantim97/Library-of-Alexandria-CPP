e8dfbc5edcc9e8e9f5e99664563acd8d8b302c9f
Tim <timothycoelho@gmail.com>
2024-07-28 19:10:35 -0400

    Mediator and Observer Patterns fixed.

--- Design Patterns/Mediator/chat_room.cpp
+++ Design Patterns/Mediator/chat_room.cpp
@@ -1,18 +1,107 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <map>
+#include <string>
 #include <memory>
-#include <functional>
-#include <algorithm>
-#include <fstream>
-#include <sstream>
-#include <cmath>
-#include <cstdio>
-#include <cstdint>
-#include <tuple>
+
+// Forward declaration
+class User;
+
+class ChatRoom
+{
+private:
+	std::vector<std::shared_ptr<User>> users;
+
+public:
+	void join(const std::shared_ptr<User>& user);
+	void broadcast(const std::string& from, const std::string& message);
+	void message(const std::string& from, const std::string& to, const std::string& message);
+};
+
+class User
+{
+private:
+	std::string name_;
+	ChatRoom* chat_room_ = nullptr;
+
+public:
+	explicit User(const std::string& name) : name_(name) {}
+
+	void receive(const std::string& from, const std::string& message)
+	{
+		std::cout << name_ << " received a message from " << from << ": " << message << std::endl;
+	}
+
+	void send(const std::string& message)
+	{
+		if (chat_room_)
+		{
+			chat_room_->broadcast(name_, message);
+		}
+	}
+
+	void send_to(const std::string& to, const std::string& message)
+	{
+		if (chat_room_)
+		{
+			chat_room_->message(name_, to, message);
+		}
+	}
+
+	void set_chat_room(ChatRoom* room)
+	{
+		chat_room_ = room;
+	}
+
+	[[nodiscard]] std::string get_name() const
+	{
+		return name_;
+	}
+};
+
+void ChatRoom::join(const std::shared_ptr<User>& user)
+{
+	users.push_back(user);
+	user->set_chat_room(this);
+}
+
+void ChatRoom::broadcast(const std::string& from, const std::string& message)
+{
+	for (const std::shared_ptr<User>& user : users)
+	{
+		if (user->get_name() != from)
+		{
+			user->receive(from, message);
+		}
+	}
+}
+
+void ChatRoom::message(const std::string& from, const std::string& to, const std::string& message)
+{
+	for (const std::shared_ptr<User>& user : users)
+	{
+		if (user->get_name() == to)
+		{
+			user->receive(from, message);
+			break;
+		}
+	}
+}
 
 int main()
 {
-    return 0;
+	std::shared_ptr<ChatRoom> chat_room = std::make_shared<ChatRoom>();
+
+	std::shared_ptr<User> alice = std::make_shared<User>("Alice");
+	std::shared_ptr<User>  bob = std::make_shared<User>("Bob");
+	std::shared_ptr<User>  charlie = std::make_shared<User>("Charlie");
+
+	chat_room->join(alice);
+	chat_room->join(bob);
+	chat_room->join(charlie);
+
+	alice->send("Hello, everyone!");
+	bob->send("Hi, Alice!");
+	charlie->send_to("Alice", "Hey Alice, how are you?");
+
+	return 0;
 }
--- Design Patterns/Observer/observer.cpp
+++ Design Patterns/Observer/observer.cpp
@@ -1,88 +1,88 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-class Person : public Observable<Person>//observable = being monitored
-{
-    int age;
-public:
-    Person(int age) : age(age) {}
-
-    int get_age() const
-    {
-        return age;
-    }
-
-    void set_age(int age)
-    {
-        if (age == Person::age)
-        {
-            return;
-        }
-        notify(*this, "age");
-    }
-};
-
-//CRTP
-template <typename T>
-class Observable
-{
-    std::vector<Observer<T>*> observers;
-public:
-    void notify(T& src, const std::string& field_name)
-    {
-        for (auto observer : observers)
-        {
-            observer->field_changed(src, field_name);
-        }
-    }
-
-    void subscribe(Observer<T>& observer)
-    {
-        observers.push_back(&observer);
-    }
-
-    void unsubscribe(Observer<T>& observer)
-    {
-        observers.erase(remove(observers.begin(), observers.end(), observer), observers.end());
-    }
-};
-
-//observer = monitoring
+#include <algorithm>
 
 template <typename T>
 class Observer
 {
-    virtual void field_changed(T& src, const std::string field_name) = 0;
+public:
+	virtual void field_changed(T& src, const std::string& field_name) = 0;
 };
 
-struct ConsolePersonObserver : public Observer<Person>
+template <typename T>
+class Observable
 {
-    private:
-        void field_change(Person& src, const std::string field_name)
-        {
-            std::cout << "Person's " << field_name << " has changed to ";
-            if (field_name == "age")
-            {
-                std::cout << src.get_age();
-            }
+	std::vector<Observer<T>*> observers;
+public:
+	void notify(T& src, const std::string& field_name)
+	{
+		for (auto observer : observers)
+		{
+			observer->field_changed(src, field_name);
+		}
+	}
 
-            std::cout << '\n';
-        }
+	void subscribe(Observer<T>& observer)
+	{
+		observers.push_back(&observer);
+	}
+
+	void unsubscribe(Observer<T>& observer)
+	{
+		observers.erase(std::remove(observers.begin(), observers.end(), &observer), observers.end());
+	}
+};
+
+class Person : public Observable<Person> // Person is now observable
+{
+	int age;
+public:
+	explicit Person(int age) : age(age) {}
+
+	[[nodiscard]] int get_age() const
+	{
+		return age;
+	}
+
+	void set_age(int a)
+	{
+		if (this->age == a)
+		{
+			return;
+		}
+		this->age = a;
+		notify(*this, "a");
+	}
+};
+
+class ConsolePersonObserver : public Observer<Person>
+{
+public:
+	void field_changed(Person& src, const std::string& field_name) override
+	{
+		std::cout << "Person's " << field_name << " has changed to ";
+		if (field_name == "age")
+		{
+			std::cout << src.get_age();
+		}
+
+		std::cout << '\n';
+	}
 };
 
 int main()
 {
-    Person p{10};
-    ConsolePersonObserver cpo;
-    p.subscribe(cpo);
+	Person p{10};
+	ConsolePersonObserver cpo;
+	p.subscribe(cpo);
 
-    p.set_age(20);
-    p.set_age(30);
+	p.set_age(20);
+	p.set_age(30);
 
-    p.unsubscribe(cpo);
+	p.unsubscribe(cpo);
 
-    p.set_age(50);
+	p.set_age(50);
 
-    return 0;
+	return 0;
 }
