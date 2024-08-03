cf38c11a00f334ef7c5fb45f4620becc9c77032d
Tim <timothycoelho@gmail.com>
2024-08-03 09:47:04 -0400

    Blockchain Example Updated.

--- Data Structures/Blockchain/Block.cpp
+++ Data Structures/Blockchain/Block.cpp
@@ -1,9 +1,9 @@
 #include "Block.h"
 #include <ctime>
 #include <sstream>
-#include "sha256.h"
+#include "Encryption/SHA256/sha256.h"
 
-Block::Block(uint32_t index_in, const std::string& data_in)
+Block::Block(const uint32_t index_in, const std::string& data_in) : _index(index_in), _data(data_in)
 {
 	_nonce = -1;
 	_time = time(0);
@@ -27,7 +27,7 @@ void Block::mine_block(uint32_t difficulty)
 
 	std::string str(cstr);
 
-	delete cstr;
+	delete[] cstr;
 	
 	do
 	{
@@ -41,7 +41,7 @@ void Block::mine_block(uint32_t difficulty)
 std::string Block::_calculate_hash() const
 {
 	std::stringstream ss;
-	ss << _index << _time << _data << _nonce << prevHash;
+	ss << _index << _time << _data << _nonce << prev_hash;
 
 	return sha256(ss.str());
 }
--- Data Structures/Blockchain/blockchain_driver.cpp
+++ Data Structures/Blockchain/blockchain_driver.cpp
@@ -2,16 +2,16 @@
 
 int main()
 {
-	Blockchain bChain = Blockchain();
+	Blockchain chain = Blockchain(6);
 
 	std::cout << "Mining block 1...\n";
-	bChain.addBlock(Block(1, "Block 1 Data"));
+	chain.add_block(Block(1, "Block 1 Data"));
 
 	std::cout << "Mining block 2...\n";
-	bChain.addBlock(Block(2, "Block 2 Data"));
+	chain.add_block(Block(2, "Block 2 Data"));
 
 	std::cout << "Mining block 3...\n";
-	bChain.addBlock(Block(3, "Block 3 Data"));
+	chain.add_block(Block(3, "Block 3 Data"));
 	
 	return 0;
 }
