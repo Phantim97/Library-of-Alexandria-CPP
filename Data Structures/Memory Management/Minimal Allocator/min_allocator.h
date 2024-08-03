9621295f0664308e923cd953ba3324c24fe6cded
Tim <timothycoelho@gmail.com>
2024-08-03 10:39:03 -0400

    Minimal Allocator Example Updated.

--- Data Structures/Memory Management/Minimal Allocator/min_allocator.h
+++ Data Structures/Memory Management/Minimal Allocator/min_allocator.h
@@ -1,15 +1,57 @@
+#pragma once
+#include <cstddef>
+#include <cstdlib>
+#include <new>
+#include <numeric>
+
 template<typename T>
 struct Alloc
 {
 	using value_type = T;
-	Alloc();
 
-	template<typename U> Alloc(const Alloc<U>&);
-	T* allocate(size_t n);
-	void deallocate(T*, size_t) const noexcept;
+	Alloc() noexcept = default;
+
+	template<typename U>
+	Alloc(const Alloc<U>&) noexcept {}
+
+	// Allocates memory for n objects of type T
+	T* allocate(std::size_t n)
+	{
+		if (n == 0)
+		{
+			return nullptr;
+		}
+
+		if (n > std::numeric_limits<std::size_t>::max() / sizeof(T))
+		{
+			throw std::bad_array_new_length{};
+		}
+
+		void* ptr = std::malloc(n * sizeof(T));
+		if (!ptr)
+		{
+			throw std::bad_alloc{};
+		}
+
+		return static_cast<T*>(ptr);
+	}
+
+	// Deallocates the memory pointed to by p
+	void deallocate(T* p, std::size_t) noexcept
+	{
+		std::free(p);
+	}
+
+	// Comparison operators
+	template<typename U>
+	bool operator==(const Alloc<U>&) const noexcept
+	{
+		return true;
+	}
+
+	template<typename U>
+	bool operator!=(const Alloc<U>&) const noexcept
+	{
+		return false;
+	}
 };
-
-template<typename T>
-bool oeprator == (const Alloc<T>&, const Alloc<T>&);
-template<typename T>
-bool oeprator != (const Alloc<T>&, const Alloc<T>&);
