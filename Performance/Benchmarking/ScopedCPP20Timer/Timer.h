580fc35b76139127f9681ba75276f70b846b68ff
Tim <timothycoelho@gmail.com>
2024-10-04 01:21:14 -0400

    Patched out TimerRes.

--- Performance/Benchmarking/ScopedCPP20Timer/Timer.h
+++ Performance/Benchmarking/ScopedCPP20Timer/Timer.h
@@ -16,35 +16,6 @@ concept TimeType = std::is_same_v<T, std::chrono::nanoseconds>
                    || std::is_same_v<T, std::chrono::minutes>
                    || std::is_same_v<T, std::chrono::hours>;
 
-template <TimeType T>
-struct TimerRes
-{
-	void operator()(const int t) const
-	{
-		//Compile Time Branching
-		if constexpr (std::is_same_v<T, std::chrono::nanoseconds>)
-		{
-			std::cout << "Duration: " << t << "ns" << '\n';
-		}
-		else if constexpr (std::is_same_v<T, std::chrono::milliseconds>)
-		{
-			std::cout << "Duration: " << t << "ms" << '\n';
-		}
-		else if constexpr (std::is_same_v<T, std::chrono::seconds>)
-		{
-			std::cout << "Duration: " << t << "s" << '\n';
-		}
-		else if constexpr (std::is_same_v<T, std::chrono::minutes>)
-		{
-			std::cout << "Duration: " << t << "mins" << '\n';
-		}
-		else if constexpr (std::is_same_v<T, std::chrono::hours>)
-		{
-			std::cout << "Duration: " << t << "hrs" << '\n';
-		}
-	}
-};
-
 //SFINAE's out any non-time types
 template<TimeType T>
 class Timer
@@ -52,17 +23,9 @@ class Timer
 private:
 	std::chrono::high_resolution_clock::time_point t0_;
 	std::chrono::high_resolution_clock::time_point t1_;
-	std::function<void(int)> callback_;
 
 public:
-	Timer() : t0_(std::chrono::high_resolution_clock::now()),
-	          callback_(TimerRes<T>())
-	{
-	}
-
-	Timer(std::function<void(int)> callback) :
-			t0_(std::chrono::high_resolution_clock::now()),
-			callback_(std::move(callback))
+	Timer() : t0_(std::chrono::high_resolution_clock::now())
 	{
 	}
 
@@ -71,7 +34,27 @@ public:
 		t1_ = std::chrono::high_resolution_clock::now();
 		const int elapsed = std::chrono::duration_cast<T>(t1_ - t0_).count();
 
-		callback_(elapsed);
+		//Compile Time Branching
+		if constexpr (std::is_same_v<T, std::chrono::nanoseconds>)
+		{
+			std::cout << "Duration: " << elapsed << "ns" << '\n';
+		}
+		else if constexpr (std::is_same_v<T, std::chrono::milliseconds>)
+		{
+			std::cout << "Duration: " << elapsed << "ms" << '\n';
+		}
+		else if constexpr (std::is_same_v<T, std::chrono::seconds>)
+		{
+			std::cout << "Duration: " << elapsed << "s" << '\n';
+		}
+		else if constexpr (std::is_same_v<T, std::chrono::minutes>)
+		{
+			std::cout << "Duration: " << elapsed << "mins" << '\n';
+		}
+		else if constexpr (std::is_same_v<T, std::chrono::hours>)
+		{
+			std::cout << "Duration: " << elapsed << "hrs" << '\n';
+		}
 	}
 };
 
